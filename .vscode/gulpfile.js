92eb8ac5c7760a548f105dc0bf7ed74e13f7bb48
Vedran Mandić <vedran@mogy.me>
2016-08-11 16:16:01 +0200

    finished, responsive

--- .vscode/gulpfile.js
+++ .vscode/gulpfile.js
@@ -1,6 +1,8 @@
 // Less configuration
 var gulp = require('gulp');
 var less = require('gulp-less');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
 
 gulp.task('less', function() {
     gulp.src('*.less')
@@ -12,4 +14,15 @@ gulp.task('less', function() {
 
 gulp.task('default', ['less'], function() {
     gulp.watch('*.less', ['less']);
+    console.log("Lessifyed...");
+    
+    gulp.watch('*.css', ['minify-css']);
+    console.log("CSS minified...");
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('../styles.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('..'));
 });
