b32036ff84b789628a9b90a2184957c7dd9bec8e
Vedran Mandić <vedran@mogy.me>
2016-08-12 17:04:31 +0200

    node added, metas added, typo fixes, gym images added

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-The system cannot find the file specified.
+var express = require("express");
+var app = express();
+var path = require("path");
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/rob-macintyre.html'));
+});
+
+app.get('/styles.min.css', function (req, res) {
+    res.sendFile(path.join(__dirname + '/styles.min.css'));
+});
+
+app.use("/img", express.static(__dirname + '/img'));
+
+app.listen(3000);
+console.log("Running at Port 3000");
